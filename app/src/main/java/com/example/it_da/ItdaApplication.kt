e573f864ef99bb766c49134cb82235ab627bccce
cnsvkf <ttagno4230@gmail.com>
2026-05-30 21:06:26 +0900

    build(gradle): Hilt 및 앱 기반 의존성 추가

--- app/src/main/java/com/example/it_da/ItdaApplication.kt
+++ app/src/main/java/com/example/it_da/ItdaApplication.kt
@@ -2,7 +2,9 @@ package com.example.it_da
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class ItdaApplication : Application() {
     // Initializes Kakao SDK once when the app process starts if a native app key is configured.
     override fun onCreate() {
